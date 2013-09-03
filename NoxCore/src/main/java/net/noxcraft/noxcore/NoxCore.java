2f2fd4821c3f760e806be0cef1238607e4e00611
Christopher Krier <ckrier.3000@gmail.com>
2013-09-03 15:23:54 -0400

    Add permissions to home system
    Permissions:
    homes.*
    	homes.admin.*
    		homes.admin.load
    		homes.admin.save
    		homes.admin.reload
    		homes.admin.wipe
    		homes.admin.config
    	homes.history.*
    		homes.history.read
    		homes.history.clear
    	homes.sethome.*
    		homes.sethome.named
    		homes.sethome
    		homes.sethome.*.anywhere
    		homes.sethome.anywhere
    	homes.homelimit.unlimited
    	homes.sethome.other.*
    		homes.sethome.other.named
    		homes.sethome.other
    		homes.sethome.other.*.anywhere
    		homes.sethome.other.anywhere
    	homes.delhome.*
    		homes.delhome.named
    		homes.delhome
    	homes.delhome.other.*
    		homes.delhome.other.named
    		homes.delhome.other
    	homes.getloc.*
    		homes.getloc.named
    		homes.getloc
    	homes.getloc.other.*
    		homes.getloc.other.named
    		homes.getloc.other

--- NoxCore/src/main/java/net/noxcraft/noxcore/NoxCore.java
+++ NoxCore/src/main/java/net/noxcraft/noxcore/NoxCore.java
@@ -47,6 +47,39 @@ public class NoxCore extends PluginBase {
 				new NoxcraftPermission("core.save", "Save permission for saving everything in core.", PermissionDefault.OP),
 				new NoxcraftPermission("core.load", "Load permission for loading everything in core.", PermissionDefault.OP)
 				));
+		addPermission(new NoxcraftPermission("homes.*", "All homes permissions including admin nodes!", PermissionDefault.OP ,
+				new NoxcraftPermission("homes.admin.*", "All admin nodes." , PermissionDefault.OP,
+						new NoxcraftPermission("homes.admin.load", "Load file permission node (Includes reloads)", PermissionDefault.OP),
+						new NoxcraftPermission("homes.admin.save", "Save to file permission node", PermissionDefault.OP),
+						new NoxcraftPermission("homes.admin.wipe", "Wipe all homes!", PermissionDefault.OP),
+						new NoxcraftPermission("homes.admin.config", "Edit config options ingame.", PermissionDefault.OP)
+				
+						),
+				new NoxcraftPermission("homes.history.*", "History nodes. Useful for mods. DOES contains admin nodes.", PermissionDefault.OP,
+						new NoxcraftPermission("homes.history.read", "Permission to read home history.", PermissionDefault.OP),
+						new NoxcraftPermission("homes.history.clear", "Permission to clear history.", PermissionDefault.OP)),
+				new NoxcraftPermission("homes.sethome.*", "Permission to all sethome nodes.", PermissionDefault.OP,
+						new NoxcraftPermission("homes.sethome.named", "Set named homes.", PermissionDefault.OP),
+						new NoxcraftPermission("homes.sethome.*.anywhere", "Set homes anywhere.", PermissionDefault.OP),
+						new NoxcraftPermission("homes.sethome.anywhere", "Set homes anywhere.", PermissionDefault.OP)),
+				new NoxcraftPermission("homes.homelimit.unlimited", "Have unlimited homes.", PermissionDefault.OP),
+				new NoxcraftPermission("homes.sethome.other.*", "Permission to all sethome nodes.", PermissionDefault.OP,
+						new NoxcraftPermission("homes.sethome.other.named", "Set named homes.", PermissionDefault.OP),
+						new NoxcraftPermission("homes.sethome.other.*.anywhere", "Set homes anywhere.", PermissionDefault.OP),
+						new NoxcraftPermission("homes.sethome.other.anywhere", "Set homes anywhere.", PermissionDefault.OP)),
+				new NoxcraftPermission("homes.delhome.*", "Permission to delete any of your own homes.", PermissionDefault.OP, 
+						new NoxcraftPermission("homes.delhome.named", "Permission to delete a named home", PermissionDefault.OP),
+						new NoxcraftPermission("homes.delhome", "Permission delete your own default home.", PermissionDefault.OP)),
+				new NoxcraftPermission("homes.delhome.other.*", "Permission to delete anyones homes of any type.", PermissionDefault.OP,
+						new NoxcraftPermission("homes.delhome.other.named", "Permission to deleted named homes by others", PermissionDefault.OP),
+						new NoxcraftPermission("homes.delhome.other", "Permission to delete default homes of others.", PermissionDefault.OP)),
+				new NoxcraftPermission("homes.getloc.*", "Permission to get locations of your homes.", PermissionDefault.OP, 
+						new NoxcraftPermission("homes.getloc.named", "Get co ords to named home.", PermissionDefault.OP),
+						new NoxcraftPermission("homes.getloc", "Get default co ords to home.", PermissionDefault.OP)),
+				new NoxcraftPermission("homes.getloc.other.*", "Get position of others homes.", PermissionDefault.OP,
+						new NoxcraftPermission("homes.getloc.other.named", "Get others locations of named homes", PermissionDefault.OP),
+						new NoxcraftPermission("homes.getloc.other", "Get default home location of others.", PermissionDefault.OP))
+				));
 	}
 	
 	public void addPermission(NoxcraftPermission permission)
