ad2288464abf5e527d4547f74997fab35dd2470a
Christopher Krier <ckrier.3000@gmail.com>
2013-10-03 15:57:37 -0400

    Create NoxMMO Project.
    OFFICIALLY.

---
+++ NoxMMO/src/main/java/com/noxpvp/mmo/NoxMMO.java
@@ -0,0 +1,40 @@
+package com.noxpvp.mmo;
+
+import java.util.logging.Level;
+
+import org.bukkit.command.CommandSender;
+
+import com.noxpvp.core.NoxPlugin;
+
+public class NoxMMO extends NoxPlugin {
+	private static NoxMMO instance;
+	
+	@Override
+	public boolean command(CommandSender arg0, String arg1, String[] arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void disable() {
+		setInstance(null);
+	}
+
+	@Override
+	public void enable() {
+		if (instance != null)
+		{
+			log(Level.SEVERE, "This plugin already has an instance running!! Disabling second run.");
+			setEnabled(false);
+			return;
+		}
+		
+		setInstance(this);
+	}
+	
+	private void setInstance(NoxMMO noxMMO) {
+		instance = noxMMO;
+	}
+
+	public static NoxMMO getInstance() { return instance; } 
+}
