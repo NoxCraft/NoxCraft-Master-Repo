a058f77cc46b5b8ec8b8a8c3953559ce462d39bf
Christopher Krier <ckrier.3000@gmail.com>
2013-10-11 18:17:33 -0400

    Add some partialy implemented files.

---
+++ NoxMMO/src/main/java/com/noxpvp/mmo/abilities/entity/ForcePullAbility.java
@@ -0,0 +1,23 @@
+package com.noxpvp.mmo.abilities.entity;
+
+import org.bukkit.entity.Entity;
+
+import com.noxpvp.mmo.abilities.BaseEntityAbility;
+
+public class ForcePullAbility extends BaseEntityAbility {
+
+	public ForcePullAbility(Entity ent)
+	{
+		super("ForcePullAbility", ent);
+	}
+	
+	public boolean execute() {
+		return false;
+	}
+
+	public boolean mayExecute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ NoxMMO/src/main/java/com/noxpvp/mmo/abilities/player/SuperBreakerAbility.java
@@ -0,0 +1,50 @@
+package com.noxpvp.mmo.abilities.player;
+
+import org.bukkit.entity.Player;
+
+import com.noxpvp.core.data.NoxPlayerAdapter;
+import com.noxpvp.mmo.abilities.BasePlayerAbility;
+
+public class SuperBreakerAbility extends BasePlayerAbility {
+	private static long maximumTicks = 6000;
+	
+	/**
+	 * @return the maximumTicks
+	 */
+	public static final long getMaximumTicks() {
+		return maximumTicks;
+	}
+
+	/**
+	 * @param maximumTicks the maximumTicks to set
+	 */
+	public static final void setMaximumTicks(long maximumTicks) {
+		SuperBreakerAbility.maximumTicks = maximumTicks;
+	}
+
+	public SuperBreakerAbility(Player player)
+	{
+		super("SuperBreakerAbility", player);
+	}
+	
+	public SuperBreakerAbility(NoxPlayerAdapter adapt)
+	{
+		this(adapt.getNoxPlayer().getPlayer());
+	}
+	
+	public boolean execute() {
+		Player player = getPlayer();
+		if (player == null)
+			return false;
+		
+		
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean mayExecute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
