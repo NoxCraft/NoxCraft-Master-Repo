9e47407a0493503ce688df06d675e2b52f628333
Connor Stone <connor@connor-sotone.com>
2013-11-02 02:13:43 -0400

    Added HammerOfThorAbility

--- NoxMMO/src/main/java/com/noxpvp/mmo/NoxMMO.java
+++ NoxMMO/src/main/java/com/noxpvp/mmo/NoxMMO.java
@@ -9,6 +9,8 @@ import com.bergerkiller.bukkit.common.utils.StringUtil;
 import com.noxpvp.core.NoxPlugin;
 import com.noxpvp.core.permissions.NoxPermission;
 import com.noxpvp.mmo.abilities.entity.LeapAbility;
+import com.noxpvp.mmo.abilities.player.HammerOfThorAbility;
+import com.noxpvp.mmo.abilities.player.MassDestructionAbility;
 
 public class NoxMMO extends NoxPlugin {
 	public static final String PERM_NODE = "nox.mmo";
@@ -25,6 +27,9 @@ public class NoxMMO extends NoxPlugin {
 
 	@Override
 	public void disable() {
+		HammerOfThorAbility.hammerThrowers = null;
+		MassDestructionAbility.massDestructors = null;
+		
 		setInstance(null);
 	}
 
