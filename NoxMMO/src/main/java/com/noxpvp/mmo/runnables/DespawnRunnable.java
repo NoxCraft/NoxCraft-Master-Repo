5dcf3ac0617f1a4545f22395401c66de8d9d28f2
Connor Stone <Connor@connor-stone.com>
2013-11-06 22:36:33 -0500

    Fixed / Added to existing runnables
    Shock
    Despawn
    BlockTimer

--- NoxMMO/src/main/java/com/noxpvp/mmo/runnables/DespawnRunnable.java
+++ NoxMMO/src/main/java/com/noxpvp/mmo/runnables/DespawnRunnable.java
@@ -1,5 +1,8 @@
 package com.noxpvp.mmo.runnables;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.entity.Entity;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -9,20 +12,31 @@ import org.bukkit.scheduler.BukkitRunnable;
  */
 public class DespawnRunnable extends BukkitRunnable{
 	
-	private Entity e;
+	private List<Entity> entitys;
 	
 	/**
 	 * 
 	 * @param entity - The entity to mark to be removed
 	 */
 	public DespawnRunnable(Entity entity){
-		this.e = entity;
+		this.entitys = new ArrayList<Entity>();
+		this.entitys.add(entity);
+	}
+	
+	/**
+	 * 
+	 * @param entity - The entity to mark to be removed
+	 */
+	public DespawnRunnable(List<Entity> entitys){
+		this.entitys = entitys;
 	}
 	
 	public void run(){
-		if (e != null)
+		for (Entity e : this.entitys){
+			if (e == null) continue;
+				
 			e.remove();
-		else return;
+		}
 	}
 
 }
