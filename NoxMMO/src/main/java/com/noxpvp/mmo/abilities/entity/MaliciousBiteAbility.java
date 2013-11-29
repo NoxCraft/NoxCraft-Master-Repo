7acff96f478ac24aa5f409e628bdfcc98546ebd9
ConnorStone <connor@connor-stone.com>
2013-11-29 15:21:05 -0500

    Added MaliciousBite Ability
    Amend 1. Removed extra disarm ability (old original when moved to targeted). Also added permnode registration
    Amend 2. Removed extra perm nodes

---
+++ NoxMMO/src/main/java/com/noxpvp/mmo/abilities/entity/MaliciousBiteAbility.java
@@ -0,0 +1,46 @@
+package com.noxpvp.mmo.abilities.entity;
+
+import org.apache.commons.lang.math.RandomUtils;
+import org.bukkit.entity.AnimalTamer;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Tameable;
+
+import com.noxpvp.mmo.NoxMMO;
+import com.noxpvp.mmo.abilities.BaseEntityAbility;
+import com.noxpvp.mmo.abilities.PassiveAbility;
+import com.noxpvp.mmo.classes.PlayerClass;
+
+public class MaliciousBiteAbility extends BaseEntityAbility implements PassiveAbility{
+	
+	private static final String ABILITY_NAME = "Malicious bite";
+	public static final String PERM_NODE = "malicious-bite";
+	
+	private static final float damageMultiplier = 2.0f;
+
+	public MaliciousBiteAbility(Entity entity) {
+		super(ABILITY_NAME, entity);
+	}
+
+	public boolean execute() {
+		if (!mayExecute())
+			return false;
+		
+		if (!(getEntity() instanceof Tameable)) return false;
+		
+		AnimalTamer a = ((Tameable) getEntity()).getOwner();
+		
+		if (a == null || !(a instanceof Player)) return false;
+		
+		Player o = (Player) a;
+		
+		PlayerClass pClass = NoxMMO.getInstance().getPlayerManager().getMMOPlayer(o).getPlayerClass();
+		
+		return RandomUtils.nextFloat() < (pClass.getTierLevel() * pClass.getLevel()) / 1000;
+	}
+
+	public boolean mayExecute() {
+		return getEntity() != null;
+	}
+
+}
