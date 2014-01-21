14cd16af78c46dfaf9b93242fa1708574c9edeba
Christopher Krier <ckrier.3000@gmail.com>
2014-01-20 22:18:24 -0500

    Implement Some Events.
    	BankShot
    	MaliciousBite
    	AutoTools
    	BackStab
    	CriticalHit

--- NoxMMO/src/main/java/com/noxpvp/mmo/abilities/player/CriticalHitAbility.java
+++ NoxMMO/src/main/java/com/noxpvp/mmo/abilities/player/CriticalHitAbility.java
@@ -1,31 +1,40 @@
 package com.noxpvp.mmo.abilities.player;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 import com.noxpvp.mmo.MMOPlayer;
 import com.noxpvp.mmo.NoxMMO;
+import com.noxpvp.mmo.PlayerManager;
 import com.noxpvp.mmo.abilities.BasePlayerAbility;
 import com.noxpvp.mmo.abilities.PassiveAbility;
 import com.noxpvp.mmo.classes.PlayerClass;
 
-public class CriticalHitAbility extends BasePlayerAbility implements PassiveAbility{
+public class CriticalHitAbility extends BasePlayerAbility implements PassiveAbility<EntityDamageByEntityEvent> {
 
 	public static final String PERM_NODE = "Critical Hit";
 	public static final String ABILITY_NAME = "critical-hit";
-	private EntityDamageEvent event;
+	private PlayerManager pm;
 	
-	public CriticalHitAbility(Player p, EntityDamageEvent event) {
+	public CriticalHitAbility(Player p) {
 		super(ABILITY_NAME, p);
-		
-		this.event = event;
+		this.pm = NoxMMO.getInstance().getPlayerManager();
 	}
-
-	public boolean execute() { 
+	
+	public boolean execute(EntityDamageByEntityEvent event) {
 		if (!mayExecute())
 			return false;
 		
-		MMOPlayer player = NoxMMO.getInstance().getPlayerManager().getMMOPlayer(getPlayer());
+		Player playerAttacker = (Player) ((event.getDamager() instanceof Player)? event.getDamager(): null);
+		String itemName = playerAttacker.getItemInHand().getType().name();
+		
+		if (!itemName.contains("SWORD") && !itemName.contains("AXE"))
+			return false;
+		
+		if (playerAttacker == null || !playerAttacker.equals(getPlayer()))
+			return false;
+		
+		MMOPlayer player = pm.getMMOPlayer(getPlayer());
 		
 		if (player == null)
 			return false;
@@ -36,8 +45,14 @@ public class CriticalHitAbility extends BasePlayerAbility implements PassiveAbil
 		if ((Math.random() * 100) > (damage * 45)) return false;
 		
 		event.setDamage(damage);
-		
 		return true;
+	}
+	
+	/**
+	 * Always Returns True Due To Being Passive!
+	 */
+	public boolean execute() { 
+		return true; //Passive
 	}
 
 }
