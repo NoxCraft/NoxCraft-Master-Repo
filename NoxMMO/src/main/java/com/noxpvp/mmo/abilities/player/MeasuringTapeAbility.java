c66ac5f181ff28b748ea82cdaac6521edc24be06
ConnorStone <connor@connor-stone.com>
2014-03-18 21:02:00 -0400

    Ability handler fixes

--- NoxMMO/src/main/java/com/noxpvp/mmo/abilities/player/MeasuringTapeAbility.java
+++ NoxMMO/src/main/java/com/noxpvp/mmo/abilities/player/MeasuringTapeAbility.java
@@ -1,96 +1,127 @@
 package com.noxpvp.mmo.abilities.player;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Set;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.bergerkiller.bukkit.common.protocol.CommonPacket;
 import com.bergerkiller.bukkit.common.utils.PacketUtil;
+import com.noxpvp.core.utils.PlayerUtils.LineOfSightUtil;
 import com.noxpvp.mmo.NoxMMO;
 import com.noxpvp.mmo.abilities.BasePlayerAbility;
+import com.noxpvp.mmo.listeners.BaseMMOEventHandler;
+import com.noxpvp.mmo.runnables.UnregisterMMOHandlerRunnable;
 
 public class MeasuringTapeAbility extends BasePlayerAbility{
 	
 	public final static String ABILITY_NAME = "Measuring Tape";
 	public final static String PERM_NODE = "measuring-tape";
 	
-	public static Map<String, MeasuringTapeAbility> players = new HashMap<String, MeasuringTapeAbility>();
+	private BaseMMOEventHandler<PlayerInteractEvent> handler;
 	
-	private final Block b;
+	private Block[] blocks;
+	private Block b;
+	private boolean firstDone;
 
-	public MeasuringTapeAbility(Player player, Block b) {
+	public MeasuringTapeAbility(final Player player) {
 		super(ABILITY_NAME, player);
 		
-		this.b = b;
+		this.handler = new BaseMMOEventHandler<PlayerInteractEvent>(
+				new StringBuilder().append(player.getName()).append(ABILITY_NAME).append("PlayerInteractEvent").toString(),
+				EventPriority.MONITOR, 1) {
+
+					public boolean ignoreCancelled() {
+						return true;
+					}
+
+					public void execute(PlayerInteractEvent event) {
+						if (!mayExecute()){
+							unRegisterHandler(this);
+							return;
+						}
+						if (!event.getPlayer().equals(player))
+							return;
+						
+						
+						if (!firstDone){
+							b = LineOfSightUtil.getTargetBlock(player, 10, (Set<Material>) null);
+							if (b == null)
+								return;
+							
+							blocks[0] = b;
+							fakeBlock(b.getLocation(), Material.WOOL, 14);
+							
+							firstDone = true;
+						} else {
+							b = LineOfSightUtil.getTargetBlock(player, 10, (Set<Material>) null);
+							if (b == null)
+								return;
+							
+							blocks[1] = b;
+							fakeBlock(b.getLocation(), Material.WOOL, 14);
+							
+							unRegisterHandler(this);
+							Bukkit.getScheduler().runTaskLater(NoxMMO.getInstance(), new Runnable() {
+								
+								public void run() {
+									MeasuringTapeAbility.this.refreshBlocks();
+								}
+							}, 20 * 5);
+							
+						}
+						
+					}
+
+					public Class<PlayerInteractEvent> getEventType() {
+						return PlayerInteractEvent.class;
+					}
+
+					public String getEventName() {
+						return "PlayerInteractEvent";
+					}
+		};
+		
+		this.blocks = new Block[2];
 	}
 
 	public boolean execute() {
 		if (!mayExecute())
 			return false;
 		
-		final Player p = getPlayer();
-		
+		new UnregisterMMOHandlerRunnable(handler).runTaskLater(NoxMMO.getInstance(), 20 * 120);
+		return true;
+
+	}
+
+	private void fakeBlock(Location loc, Material mat, int data) {
 		CommonPacket commonFakeBlock = new CommonPacket(35, true);
 		
 		try {
-			commonFakeBlock.write(0, b.getX());
-			commonFakeBlock.write(1, b.getY());
-			commonFakeBlock.write(2, b.getZ());
-			commonFakeBlock.write(3, Material.WOOL.getId());
-			commonFakeBlock.write(4, 14);
+			commonFakeBlock.write(0, loc.getX());//need to test and use *32?
+			commonFakeBlock.write(1, loc.getY());
+			commonFakeBlock.write(2, loc.getZ());
+			commonFakeBlock.write(3, mat.getId());
+			commonFakeBlock.write(4, data);
 			
-			PacketUtil.sendPacket(p, commonFakeBlock);
-		}
-		catch (IllegalArgumentException e) {}
-		catch (Exception e) {e.printStackTrace();}
-		
-		if (MeasuringTapeAbility.players.containsKey(p.getName())){
-			final MeasuringTapeAbility first = MeasuringTapeAbility.players.get(p.getName());//save copy	
-			MeasuringTapeAbility.players.remove(p.getName());//then remove
+			PacketUtil.sendPacket(getPlayer(), commonFakeBlock);
 			
-			p.sendMessage(ChatColor.GOLD + "Distance: " + ChatColor.GREEN + (int) first.b.getLocation().distance(b.getLocation()));
+		} catch (IllegalArgumentException e) {e.printStackTrace();}
+	}
+	
+	private void refreshBlocks(){
+		for (Block b : blocks){
+			if (b == null)
+				continue;
 			
-			Bukkit.getScheduler().runTaskLater(NoxMMO.getInstance(), new Runnable() {
-				
-				private Block bTwo = first.b;
-				private World w = p.getWorld();
-				
-				public void run() {
-					CommonPacket refreshBlock = new CommonPacket(35, true),
-							refreshBlockTwo = new CommonPacket(35, true);
-					//NMSPacketPlayOutBlockAction fakeBlock = new NMSPacketPlayOutBlockAction();
-					
-					try {
-					
-					refreshBlock.write(0, b.getX());
-					refreshBlock.write(1, b.getY());
-					refreshBlock.write(2, b.getZ());
-					refreshBlock.write(3, b.getData());
-					
-					refreshBlockTwo.write(0, bTwo.getX());
-					refreshBlockTwo.write(1, bTwo.getY());
-					refreshBlockTwo.write(2, bTwo.getZ());
-					refreshBlockTwo.write(3, bTwo.getData());
-					
-					PacketUtil.sendPacket(p, refreshBlock);
-					PacketUtil.sendPacket(p, refreshBlockTwo);
-					}
-					catch (IllegalArgumentException e) {}
-					catch (Exception e) {e.printStackTrace();}
-					
-				}
-			}, 60);
-		} else {		
-			MeasuringTapeAbility.players.put(p.getName(), this);
+			fakeBlock(b.getLocation(), b.getType(), b.getData());
 		}
 		
-		return true;
 	}
 
 }
