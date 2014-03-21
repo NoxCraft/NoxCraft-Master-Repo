dc249cd349e6a7cadb50745207feec5e930edd70
ConnorStone <connor@connor-stone.com>
2014-03-20 21:46:42 -0400

    Fixes

--- NoxMMO/src/main/java/com/noxpvp/mmo/listeners/DamageListener.java
+++ NoxMMO/src/main/java/com/noxpvp/mmo/listeners/DamageListener.java
@@ -50,8 +50,10 @@ public class DamageListener extends NoxListener<NoxMMO>{
 				com.noxpvp.mmo.PlayerManager.getInstance().getPlayer(playerAttacker).setTarget(livingDamaged);
 				
 				if (getPlugin().getMMOConfig().get("effect.damage.blood", Boolean.class, Boolean.TRUE)){
-					StaticEffects.DamageAmountParticle(livingDamaged, event.getDamage());
 					StaticEffects.BloodEffect(livingDamaged, getPlugin());
+				}
+				if (getPlugin().getMMOConfig().get("effect.damage.damage-particle", Boolean.class, Boolean.TRUE)){
+					StaticEffects.DamageAmountParticle(livingDamaged, event.getDamage());
 				}
 				
 				String color = MMOLocale.GUI_BAR_COLOR.get();
