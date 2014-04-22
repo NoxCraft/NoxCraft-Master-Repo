308b07007821f64e29dc63e1ce184bf3c0617b9f
Christopher Krier <ckrier.3000@gmail.com>
2014-04-22 16:22:31 -0400

    Added the PacketWrapper module. Plus finished but needs testing for the /ability and /ability info commands.

--- NoxMMO/src/main/java/com/noxpvp/mmo/command/subcommands/AbilityInfoCommand.java
+++ NoxMMO/src/main/java/com/noxpvp/mmo/command/subcommands/AbilityInfoCommand.java
@@ -1,9 +1,18 @@
 package com.noxpvp.mmo.command.subcommands;
 
+import java.util.Map;
+
+import com.bergerkiller.bukkit.common.MessageBuilder;
 import com.noxpvp.core.commands.BaseCommand;
 import com.noxpvp.core.commands.CommandContext;
 import com.noxpvp.core.commands.NoPermissionException;
+import com.noxpvp.core.commands.SafeNullPointerException;
+import com.noxpvp.core.utils.gui.MessageUtil;
+import com.noxpvp.mmo.MMOPlayer;
 import com.noxpvp.mmo.NoxMMO;
+import com.noxpvp.mmo.PlayerManager;
+import com.noxpvp.mmo.abilities.Ability;
+import com.noxpvp.mmo.locale.MMOLocale;
 
 public class AbilityInfoCommand extends BaseCommand {
 
@@ -28,8 +37,27 @@ public class AbilityInfoCommand extends BaseCommand {
 	@Override
 	public CommandResult execute(CommandContext context)
 			throws NoPermissionException {
-		// TODO Auto-generated method stub
-		return null;
+		if (!context.hasArgument(1))
+			return new CommandResult(this, false);
+		
+		String abilityName = context.getArgument(0);
+		
+		MMOPlayer mPlayer = PlayerManager.getInstance().getPlayer(context.getPlayer());
+		
+		if (mPlayer == null)
+			return new CommandResult(this, true, new MessageBuilder().red("mPlayer object is null!").lines());
+		
+		Map<String, Ability> abilities = mPlayer.getAllMappedAbilities();
+		
+		Ability ability = null;
+		if (abilities.containsKey(abilityName))
+			ability = abilities.get(abilityName);
+		
+		if (ability == null)
+			throw new SafeNullPointerException("Ability does not exist!");
+		
+		MessageUtil.sendLocale(context.getSender(), MMOLocale.ABIL_INFO, ability.getDisplayName(), ability.getDescription());
+		return new CommandResult(this, true);
 	}
 
 	@Override
