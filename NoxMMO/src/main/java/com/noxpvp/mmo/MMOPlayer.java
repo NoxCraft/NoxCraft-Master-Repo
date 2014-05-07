65ff1964c1fee872ba44fdce092351e94c137a91
Christopher Krier <ckrier.3000@gmail.com>
2014-05-07 15:47:23 -0400

    Fix Dependency derp. Spigot-API not Spigot!

--- NoxMMO/src/main/java/com/noxpvp/mmo/MMOPlayer.java
+++ NoxMMO/src/main/java/com/noxpvp/mmo/MMOPlayer.java
@@ -7,8 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import net.minecraft.util.org.apache.commons.lang3.Validate;
-
 import org.apache.commons.lang.NotImplementedException;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
